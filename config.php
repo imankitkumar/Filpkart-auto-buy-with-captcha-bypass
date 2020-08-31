a4c6b526a58eadcfb18237864b11a452c3d32686
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-08-31 22:54:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,12 @@
 
 //set all @required values
 
-/***Product link example:
+/***
+Product link example:
 
-https://www.flipkart.com/poco-m2-pro-green-greener-64-gb/p/itm5de3b6eb57ab4?pid=MOBFT7MKFHTAPDYD&lid=LSTMOBFT7MKFHTAPDYDLRJC1B&param=7767&nnc=VF54TJL6YWGI_AD&pageUID=1598891274737***/
+https://www.flipkart.com/poco-m2-pro-green-greener-64-gb/p/itm5de3b6eb57ab4?pid=MOBFT7MKFHTAPDYD&lid=LSTMOBFT7MKFHTAPDYDLRJC1B&param=7767&nnc=VF54TJL6YWGI_AD&pageUID=1598891274737
+
+***/
 
 $set_product_link = "Place_valid_product_url_here";
 
@@ -15,8 +18,12 @@ $set_cookie = "Place_your_account_cookie_here";
 $quantity = "1";
 
 
-/***Do not change or
-modify below values***/
+/***
+
+Do not change or
+modify below values
+
+***/
 
 $parse = parse_url($set_product_link);
 
@@ -27,8 +34,12 @@ $set_product_id = $query['pid'];
 $cart_context = $query['lid'];
 
 
-/***do not change
-or modify these headers***/
+/***
+
+Do not change
+or modify these headers
+
+***/
 
 $headers = array('Content-type: application/json','X-User-Agent: Mozilla/5.0 (Linux; Android 10; MI567 Build/KQ1.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/830.90.3.96 Mobile Safari/537.36 FKUA/msite/0.0.1/msite/Mobile','Cookie: '.$set_cookie.'');
 
